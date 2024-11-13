e690935d77f913ca360f32ff75178d95b9cccb4d
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-11-14 00:39:49 +0100

    temario completado

---
+++ untitled/src/main/java/org/example/dowhil.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class dowhil {
+    public static void main(String[] args) {
+        int i=0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+    }
+}
