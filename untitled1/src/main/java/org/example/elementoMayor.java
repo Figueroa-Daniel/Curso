83592f12bb66ac2d3c75026a0aaaf57d9d2fb877
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-12-06 19:52:42 +0100

    Calculando el segundo elemento mayor

--- untitled1/src/main/java/org/example/elementoMayor.java
+++ untitled1/src/main/java/org/example/elementoMayor.java
@@ -13,21 +13,25 @@ public class elementoMayor {
         segundoMayor();
 
     }
+    //Calcular el segundo elemento mayor
     private static void segundoMayor(){
-        int [] miArray={89,37,35,5,10,2,38,8,34,3,53,36,56,78};
+        int [] miArray={93,93,90,79,89,37,35,5,10,2,38,8,34,3,53,36,56,78,91,94,94};
         int max = miArray[0];
-        int seg=;
-        for(int i=1;i<miArray.length;i++){
+        int contador=0;
+        for(int i=0;i<miArray.length;i++){
             if(miArray[i]>max){
                 max = miArray[i];
-                if(seg<=max) {
-                    seg = max;
-                }
-                if(miArray[miArray.length-1]>seg && miArray[miArray.length-1]<max) {
-                    seg = miArray[miArray.length-1];
+                contador=i;
+            }
+        }
+        int se=0;
+        for(int j=0;j< miArray.length;j++){
+            if(contador!=j){
+                if(miArray[j]>se && miArray[j]!=miArray[contador]){
+                    se= miArray[j];
                 }
             }
         }
-        System.out.println("El segundo elemento mayor del array es: "+seg);
+        System.out.println("El segundo elemento mayor del array es: "+se);
     }
 }
