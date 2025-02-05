650bdc8c6e032b485cf3da2fc2db2e7b7a58bba3
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2025-02-05 19:42:29 +0100

    sopa de letras terminada

--- JavaIntro_Cadenas/src/main/java/org/example/SopaDeLetras.java
+++ JavaIntro_Cadenas/src/main/java/org/example/SopaDeLetras.java
@@ -13,81 +13,99 @@ public class SopaDeLetras {
                 {'S', 'O', 'L', 'U', 'V', 'E'}
         };
         Scanner tec = new Scanner(System.in);
-        boolean comprobador=false;
         System.out.println("Dime la palabra que encontraste: ");
-        String palabra= tec.next();
+        String palabra = tec.next().toUpperCase();
         char[] palabraArray = palabra.toCharArray();
-        for(int i=0;i< sopaLetras.length;i++){
-            for (int j=0;j<sopaLetras[0].length;j++){
-                if(palabraArray[0]==sopaLetras[i][j]){
-                    int contador=1;
-                    while (comprobador==false || contador==palabraArray.length){
-                        int matchCase = -1;
+        boolean encontrada = false;
 
-                        if (sopaLetras[i][j - 1] == palabraArray[contador]) {
-                            matchCase = 0;
-                        } else if (sopaLetras[i][j + 1] == palabraArray[contador]) {
-                            matchCase = 1;
-                        } else if (sopaLetras[i + 1][j] == palabraArray[contador]) {
-                            matchCase = 2;
-                        } else if (sopaLetras[i - 1][j] == palabraArray[contador]) {
-                            matchCase = 3;
-                        } else if (sopaLetras[i + 1][j - 1] == palabraArray[contador]) {
-                            matchCase = 4;
-                        } else if (sopaLetras[i - 1][j - 1] == palabraArray[contador]) {
-                            matchCase = 5;
-                        } else if (sopaLetras[i - 1][j + 1] == palabraArray[contador]) {
-                            matchCase = 6;
-                        } else if (sopaLetras[i + 1][j + 1] == palabraArray[contador]) {
-                            matchCase = 7;
+        // Etiqueta para salir de todos los bucles
+        buscar:
+        for (int i = 0; i < sopaLetras.length; i++) {
+            for (int j = 0; j < sopaLetras[i].length; j++) {
+
+                if (sopaLetras[i][j] == palabraArray[0]) {
+                    // Verificar todas las direcciones posibles
+                    for (int dir = 0; dir < 8; dir++) {
+                        int contadorCoincidencias = 1;
+                        int nuevaFila = i;
+                        int nuevaCol = j;
+
+                        // Seguir la dirección actual hasta completar la palabra
+                        for (int k = 1; k < palabraArray.length; k++) {
+                            switch (dir) {
+                                case 0: // Izquierda
+                                    nuevaCol--;
+                                    break;
+                                case 1: // Derecha
+                                    nuevaCol++;
+                                    break;
+                                case 2: // Arriba
+                                    nuevaFila--;
+                                    break;
+                                case 3: // Abajo
+                                    nuevaFila++;
+                                    break;
+                                case 4: // Diagonal superior izquierda
+                                    nuevaFila--;
+                                    nuevaCol--;
+                                    break;
+                                case 5: // Diagonal superior derecha
+                                    nuevaFila--;
+                                    nuevaCol++;
+                                    break;
+                                case 6: // Diagonal inferior izquierda
+                                    nuevaFila++;
+                                    nuevaCol--;
+                                    break;
+                                case 7: // Diagonal inferior derecha
+                                    nuevaFila++;
+                                    nuevaCol++;
+                                    break;
+                            }
+
+                            // Verificar límites de la matriz
+                            if (nuevaFila < 0 || nuevaFila >= sopaLetras.length ||
+                                    nuevaCol < 0 || nuevaCol >= sopaLetras[i].length) {
+                                break;
+                            }
+
+                            // Verificar coincidencia de letra
+                            if (sopaLetras[nuevaFila][nuevaCol] == palabraArray[k]) {
+                                contadorCoincidencias++;
+                            } else {
+                                break;
+                            }
                         }
 
-                        switch (matchCase) {
-                            case 0:
-                                System.out.println("Coincidencia en la izquierda");
-                                int contador2=1;
-                                for(int w=1;w<palabraArray.length;w++){
-                                    if(sopaLetras[i][j - 1-w]==palabraArray[1+w]){
-                                        contador2++;
-                                    }
-                                    else {
-                                        break;
-                                    }
-                                    if(contador==palabraArray.length){
-                                        comprobador=true;
-                                    }
-                                }
-                                break;
-                            case 1:
-                                System.out.println("Coincidencia en la derecha");
-                                break;
-                            case 2:
-                                System.out.println("Coincidencia abajo");
-                                break;
-                            case 3:
-                                System.out.println("Coincidencia arriba");
-                                break;
-                            case 4:
-                                System.out.println("Coincidencia diagonal inferior izquierda");
-                                break;
-                            case 5:
-                                System.out.println("Coincidencia diagonal superior izquierda");
-                                break;
-                            case 6:
-                                System.out.println("Coincidencia diagonal superior derecha");
-                                break;
-                            case 7:
-                                System.out.println("Coincidencia diagonal inferior derecha");
-                                break;
-                            default:
-                                System.out.println("No hay coincidencia");
-                                break;
+                        // Si todas las letras coinciden
+                        if (contadorCoincidencias == palabraArray.length) {
+                            encontrada = true;
+                            System.out.println("¡Palabra encontrada!");
+                            System.out.println("Dirección: " + obtenerNombreDireccion(dir));
+                            System.out.println("Posición inicial: (" + i + "," + j + ")");
+                            break buscar;
                         }
                     }
                 }
             }
         }
 
+        if (!encontrada) {
+            System.out.println("La palabra no se encontró.");
+        }
     }
 
+    private static String obtenerNombreDireccion(int dir) {
+        String[] nombres = {
+                "izquierda",
+                "derecha",
+                "arriba",
+                "abajo",
+                "diagonal superior izquierda",
+                "diagonal superior derecha",
+                "diagonal inferior izquierda",
+                "diagonal inferior derecha"
+        };
+        return nombres[dir];
+    }
 }
