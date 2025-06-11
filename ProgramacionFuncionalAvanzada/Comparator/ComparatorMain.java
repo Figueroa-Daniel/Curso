450f1a6d68984fd12466db5a73919d4c1c9a2fbb
dafiv <danifv02@gmail.com>
2025-06-11 12:59:13 +0200

    terminado parte de los comparator en programacionFuncionalAvanzada

---
+++ ProgramacionFuncionalAvanzada/Comparator/ComparatorMain.java
@@ -0,0 +1,38 @@
+package ProgramacionFuncionalAvanzada.Comparator;
+
+import ProgramacionFuncionalAvanzada.Product;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorMain {
+    public static void main(String[] args) {
+        Comparator<Double> comparador1 = (n1,n2) -> n1.compareTo(n2);
+        Comparator<Double> comparador2 = Double::compareTo;
+        System.out.println(comparador1.compare(50.0, 100.0)); // -1 porque 50 es menor que 100
+        System.out.println(comparador1.compare(50.0, 50.0)); // 0 porque son iguales
+        System.out.println(comparador2.compare(100.0, 50.0)); // 1 porque 100 es mayor que 50
+
+        Product product1 = new Product("121", "pan", 10.0, false);
+        Product product2 = new Product("122", "leche", 20.0, false);
+        // Comparator<Product> productComparator = (p1, p2) -> p1.getPrice().compareTo(p2.getPrice()); esto se puede hacer de forma mas sencilla
+        Comparator<Product> productComparator = Comparator.comparing(Product::getPrice);
+         // -1 porque el precio de pan es menor que el de leche
+        System.out.println(productComparator.compare(product1, product2));
+
+        List<Product> products = new ArrayList<>(List.of(
+                new Product("123", "huevo", 4.0, false),
+                new Product("124", "queso", 15.0, false),
+                new Product("125", "jamon", 5.0, false),
+                new Product("126", "mantequilla", 30.0, false)
+        ));
+        // Ordenar productos por precio, pasando el comparator al sort
+        products.sort(productComparator);
+        System.out.println(products);
+        // Tambien se podría usar un reversed
+        products.sort(productComparator.reversed());
+        // 14:00
+
+    }
+}
---
+++ ProgramacionFuncionalAvanzada/Task.java
@@ -0,0 +1,37 @@
+package ProgramacionFuncionalAvanzada;
+
+public class Task {
+    private Long id;
+    private String title;
+    private Integer hours;
+
+    public Task(Long id, String title, Integer hours) {
+        this.id = id;
+        this.title = title;
+        this.hours = hours;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
