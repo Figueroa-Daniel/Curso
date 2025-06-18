925f3de1efe09cc1ce8efef8f7d7f67509e6dbf2
dafiv <danifv02@gmail.com>
2025-06-19 01:38:32 +0200

    avanzando con biConsumer

---
+++ InterfacesFuncionalesYLambdas/consumer/Main2.java
@@ -0,0 +1,24 @@
+package InterfacesFuncionalesYLambdas.consumer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class Main2 {
+    public static void main(String[] args) {
+        // BiConsumer es una interfaz funcional que acepta dos argumentos y no devuelve ningún resultado.
+        // Esto puede ser útil con mapas o estructuras de datos que requieren dos entradas.
+        BiConsumer<Integer,Integer> addTwo = (x, y) -> {
+            System.out.println("Suma: " + (x +" "+ y));
+        };
+        addTwo.accept(5, 10); // Imprime la concatenación de los dos números
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Uno");
+        map.put(2, "Dos");
+        map.put(3, "Tres");
+        map.forEach((key,value) -> {
+            System.out.println("Clave: " + integer + ", Valor: " + s);
+        });
+
+    }
+}
---
+++ InterfacesFuncionalesYLambdas/predicate/Main.java
@@ -0,0 +1,7 @@
+package InterfacesFuncionalesYLambdas.predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        27:00
+    }
+}
