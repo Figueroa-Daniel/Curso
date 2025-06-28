bd109e06883200c75b4e41b0287704cd9e35aa70
dafiv <danifv02@gmail.com>
2025-06-28 13:30:51 +0200

    terminado curso xml 1

---
+++ FicherosXmlEnJava/src/main/java/org/example/ProcesarXML.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class ProcesarXML {
+    // Part filePart = request.getPart("file"); Esto sirve para obtener el nombre del archivo
+    // InputStream contenidoFicheroXML = filePart.getInputStream(); Esto sirve para obtener el contenido del fichero XML
+    // DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); Esto sirve para crear un objeto DocumentBuilderFactory
+    // DocumentBuilder builder = factory.newDocumentBuilder(); Esto sirve para crear un objeto DocumentBuilder, fichero vacio
+    // Document document = builder.parse(contenidoFicheroXML); Esto sirve para parsear el contenido del fichero XML
+}
