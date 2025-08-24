d359c9df02e57bfc87003b0b5e318334f8bfa099
Figueroa <danifv02@gmail.com>
2025-08-24 13:22:45 +0200

    Delete untitled/src directory

--- untitled/src/main/java/org/example/JavaIntro_Ej5.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class JavaIntro_Ej5 {
-    public static void main(String[] args){
-    double dolares=5, euros=0.92, resultado;
-    resultado=5*euros;
-        System.out.println(resultado);
-    }
-}
